ac7d994bc30f92a8f95c39546a3e54e5b0b1f340
SrikanthMitra <curiousrikanth@gmail.com>
2023-04-05 22:43:46 +0530

    Second Day: Loops and Linear Search

--- FirstFile.py
+++ FirstFile.py
@@ -1,2 +1,24 @@
-print("Hello World")
- 
+#print("Hello World")
+
+#loops
+for i in range(0,10):
+    print(i)
+"""
+while i < 10:
+    print("Greater than 10")
+
+"""
+#Linear search
+arr = []
+for i in range(0,25,2):
+    arr.append(i)
+
+for i in arr:
+    print(i)
+
+search = 16
+for i in arr:
+    if arr[i] == search:
+        print("Match Found")
+    else:
+        print("Match not found")
